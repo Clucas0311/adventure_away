c9c465b6f2264a5694396bc85b60317758153c17
Clucas0311 <Clucas0311@gmail.com>
2022-10-20 19:55:00 -0400

    feat: register route

--- src/api/api.js
+++ src/api/api.js
@@ -11,3 +11,26 @@ export const fetchVacations = async () => {
     console.error("There was an error fetching vacations", error);
   }
 };
+
+export const registerUser = async (username, password) => {
+  try {
+    const response = await fetch(`${BASEURL}/guests/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        guest: {
+          username,
+          password,
+        },
+      }),
+    });
+    console.log("RESPONSE------>", response);
+    const data = await response.json();
+    console.log("------------data------------", data);
+    return data;
+  } catch (error) {
+    console.error("There was an error registering the user", error);
+  }
+};
