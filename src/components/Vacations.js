95ebc15b07e2265b8fb5472c789f8cc01911b713
Kiavash Page <kiavashp@users.noreply.github.com>
2022-10-29 07:27:55 -0700

    feat(search): basic filtering

--- src/components/Vacations.js
+++ src/components/Vacations.js
@@ -6,13 +6,34 @@ import "./Vacations.css";
 
 const Vacations = ({ vacation, setVacation, token }) => {
   console.log("vacations", vacation);
+  
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  const filteredVacations = vacation.filter((vacationObject) => {
+    const searchTermLower = searchTerm.toLowerCase();
+    
+    if (vacationObject.description.toLowerCase().includes(searchTermLower)) {
+      return true;
+    } else if (vacationObject.location.toLowerCase().includes(searchTermLower)) {
+      return true;
+    }
+
+    return false;
+  });
+
   return (
     <>
+      <div className="ui icon input">
+        <input type="text" placeholder="Search"
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)} />
+          <i className="search icon"></i>
+      </div>
       <Link to="/vacations/create" className="ui button">
         Create Vacation
       </Link>
       <div className="vacations-container">
-        {vacation.map((item) => {
+        {filteredVacations.map((item) => {
           return (
             <VacationItem
               key={item.id}
